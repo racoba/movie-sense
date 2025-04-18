7cfd807df7bdc2d88875d4ded14f9678e0acdf98
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2025-04-18 11:07:58 -0300

    fixed gallery page

--- movie-sense/src/app/gallery/page.tsx
+++ movie-sense/src/app/gallery/page.tsx
@@ -1,10 +1,19 @@
 import { MovieCard } from "@/components";
+import { movies } from "@/resources/variables";
 
 const Gallery = () => {
     return (
         <div className="flex flex-col gap-10 lg:px-8">
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
-                <MovieCard name="MAGICO DE OZ" rate={7} year={2002} />
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-6">
+                    {movies.map((movie, index) => (
+                        <MovieCard
+                            key={index}
+                            name={movie.title}
+                            rate={movie.rate}
+                            year={movie.year}
+                            src={movie.image}
+                        />
+                    ))}
             </div>
         </div>
     )
