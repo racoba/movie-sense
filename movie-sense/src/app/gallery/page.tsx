359d2eeb6dd85e45e60ec26162294c07aade9e5d
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2025-04-21 13:25:43 -0300

    added loading suspense

--- movie-sense/src/app/gallery/page.tsx
+++ movie-sense/src/app/gallery/page.tsx
@@ -1,39 +1,45 @@
-"use client"
+"use client";
 
+import { Suspense } from "react";
 import { MovieCard } from "@/components";
 import { movies } from "@/resources/variables";
 import { useSearchParams } from "next/navigation";
 
-const Gallery = () => {
+const GalleryContent = () => {
     const searchParams = useSearchParams();
-    const movieFilter = searchParams.get("filter")
+    const movieFilter = searchParams.get("filter");
 
     const getFilteredMovies = () => {
         const filteredMovies = movies.filter((movie) => movie.theme == movieFilter);
-
-        if (filteredMovies.length > 0) return filteredMovies
-        if (movieFilter) alert(`No ${movieFilter} movies`)
-        return movies
-    }
+        if (filteredMovies.length > 0) return filteredMovies;
+        if (movieFilter) alert(`No ${movieFilter} movies`);
+        return movies;
+    };
 
     return (
         <div className="flex flex-col gap-10 lg:px-8">
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-6">
-                {getFilteredMovies().map((movie, index) => {
-                    return (
-                        <MovieCard
-                            key={index}
-                            name={movie.title}
-                            rate={movie.rate}
-                            year={movie.year}
-                            src={movie.src}
-                            theme={movie.theme}
-                        />
-                    )
-                })}
+                {getFilteredMovies().map((movie, index) => (
+                    <MovieCard
+                        key={index}
+                        name={movie.title}
+                        rate={movie.rate}
+                        year={movie.year}
+                        src={movie.src}
+                        theme={movie.theme}
+                    />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Gallery;
+const GalleryPage = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <GalleryContent />
+        </Suspense>
+    );
+};
+
+export default GalleryPage;
